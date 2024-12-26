c645a3f07bb7916b9a6d876135f7412d10ff6fe5
Facundo Gracia <facundogracia01@gmail.com>
2024-12-26 19:00:13 -0300

    Terminado

---
+++ JavaSc/main.js
@@ -0,0 +1,30 @@
+// Buscamos los datos a traves del id
+const info = document.getElementById('info');
+const resultado = document.getElementById('resultado');
+// Intereses
+const intereses = {
+  1: 0.05,
+  3: 0.10,
+  6: 0.15,
+  9: 0.20,
+  12: 0.25
+};
+
+info.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const monto = parseFloat(document.getElementById('monto').value);
+  const cuotas = parseInt(document.getElementById('cuotas').value);
+  const interes = intereses[cuotas] || 0;
+  const cuotaMensual = ((monto * (1 + interes)) / cuotas).toFixed(2); //Utilizo el tofixed para nada mas mostrar 2 decimales.
+  mostrarResultado(monto, cuotas, interes, cuotaMensual);
+});
+// Funcion para mostrar la información con sus montos
+function mostrarResultado(monto, cuotas, interes, cuotaMensual) {
+  resultado.innerHTML = `
+    <h2>Resultado</h2>
+    <p>Monto del préstamo: $${monto}</p>
+    <p>Número de cuotas: ${cuotas}</p>
+    <p>Interés: ${interes * 100}%</p>
+    <p>Cuota mensual: $${cuotaMensual}</p>
+  `;
+}
